1fbd99e39de770158713b9afd5802767bbf776a1
Michael Gattozzi <mgattozzi@gmail.com>
2016-10-15 21:07:59 -0400

    Remove unnecessary c code

--- htest/hsbracket.c
+++ htest/hsbracket.c
@@ -1,14 +1,1 @@
 #include <HsFFI.h>
-
-static void my_enter(void)
-{
-  static char *argv[] = { "libhtest.so", 0 }, **argv_ = argv;
-  static int argc = 1;
-  hs_init(&argc, &argv_);
-}
-
-static void my_exit(void)
-{
-  hs_exit();
-}
-
